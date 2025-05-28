9293017aed39d5ee79f11fa9b015fc971e6de737
Carlos Estay <cestay@redhat.com>
2025-05-28 17:28:06 -0400

    updated

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@
 
 
 ## GitLab
-helm uninstall gitlab demo-operator-gitlab --namespace gitlab-system
-helm uninstall gitlab-operator gitlab-operator/gitlab-operator --namespace gitlab-system
+helm uninstall gitlab --namespace gitlab-system
+helm uninstall gitlab-operator --namespace gitlab-system
 oc delete project gitlab-system
 helm repo remove gitlab-operator
